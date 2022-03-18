812420a37ce3319f2ad4e520232e7bc29769c4d9
Ourai Lin <ourairyu@gmail.com>
2022-03-18 11:21:38 +0800

    feat: `onSelect` event prop of `DropdownMenu` component

--- src/dropdown/dropdown/logic.ts
+++ src/dropdown/dropdown/logic.ts
@@ -4,7 +4,7 @@ import { Component } from 'vue-property-decorator';
 import ElDropdown from 'element-ui/lib/dropdown';
 import ElDropdownMenu from 'element-ui/lib/dropdown-menu';
 
-import { isSpecificComponent } from '@kokiri/core/dist/basic';
+import { noop, isSpecificComponent } from '@kokiri/core/dist/basic';
 import { DropdownStructuralComponent } from '@kokiri/core/dist/dropdown';
 
 import { getComponentName, convertPlacement } from '../../basic';
@@ -20,6 +20,7 @@ export default class Dropdown extends DropdownStructuralComponent {
     const children: VNode[] = [];
 
     let dropdownMenu: VNode | undefined;
+    let dropdownMenuSelectListener: ((flag: string) => void) | undefined;
 
     (this.$slots.default || []).forEach(vn => {
       if (isSpecificComponent(vn, getComponentName('dropdownMenu'))) {
@@ -30,13 +31,12 @@ export default class Dropdown extends DropdownStructuralComponent {
     });
 
     if (dropdownMenu) {
-      children.push(
-        h(
-          ElDropdownMenu,
-          { slot: 'dropdown' },
-          dropdownMenu.componentOptions ? dropdownMenu.componentOptions.children || [] : [],
-        ),
-      );
+      const { children: dropdownMenuItems = [], listeners = {} } =
+        dropdownMenu.componentOptions || {};
+
+      dropdownMenuSelectListener = (listeners as Record<string, any>).select;
+
+      children.push(h(ElDropdownMenu, { slot: 'dropdown' }, dropdownMenuItems));
     }
 
     return h(
@@ -44,6 +44,7 @@ export default class Dropdown extends DropdownStructuralComponent {
       {
         class: this.className,
         props: { trigger: this.trigger, placement: convertPlacement(this.placement) },
+        on: { command: dropdownMenuSelectListener || noop },
       },
       children,
     );
